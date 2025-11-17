656853aee2d3305cc19de82318703c04a0fa48cb
GNarvaez <ganarvaez@uamv.edu.ni>
2025-11-17 09:06:38 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoGrafos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoGrafos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
